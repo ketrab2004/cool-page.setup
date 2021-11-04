5d23990ccb7ae7dcfbfeb4957c831d4b65e0e6b2
Bartek O <bartek@fos.nl>
2021-11-04 16:15:29 +0100

    Created example mysql script

---
+++ database.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------
+-- Schema setup
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `cool-page-setup` ;
+
+CREATE SCHEMA IF NOT EXISTS `cool-page-setup` DEFAULT CHARACTER SET utf8 ;
+USE `cool-page-setup` ;
+
+
+-- -----------------------------------------------------
+-- Table `cool-page-setup`.`example`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `cool-page-setup`.`example`;
+
+CREATE TABLE IF NOT EXISTS `cool-page-setup`.`example`
+(
+    `id`        INT             AUTO_INCREMENT, 
+    `content`   VARCHAR(64)     NULL,
+
+    PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
+
